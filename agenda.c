6af8ec58cbdb798101057d034701bf3e76bc62d3
zPiras <72572786+zPiras@users.noreply.github.com>
2021-03-29 15:39:48 -0300

    Update agenda.c

--- agenda.c
+++ agenda.c
@@ -1,87 +1,147 @@
-#include <math.h>
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-#define VET_Person 10
+#define MAX 100
 
-typedef struct Pessoa {
-    int codigo[50];
-    char nome[50];
-    char data[8];
-    char fone[19];
-    char email[50];
-    char obseracao[100];
-} Pessoa;
+struct Pessoa{
+    char* nome;
+    int idade;
+    char telefone[30];
+    char* email;
+    char* obervacao;
+}pessoa[MAX];
 
-void menu(){
-    printf("\n-------------AGENDA--------------");
-    printf("\n01- Cadatrar contato");
-    printf("\n02- Listar contato");
-    printf("\n03- Sair\n");
-    
+void exibirMenu(){
+    printf("\n\tSelecione umas das opcoes a seguir:\n");
+    printf("\t01 - Cadastrar contato\n"
+           "\t02 - Listar contatos\n"
+           "\t03 - Pesquisar contato\n"
+           "\t04 - Sair\n\n");
 }
 
-int cad(Pessoa pessoa[VET_Person], int count) {
-    int numero;
+void exibirContato(int indice){
+    printf("\n\tNome: %s\n", pessoa[indice].nome);
+    printf("\tIdade: %d\n", pessoa[indice].idade);
+    printf("\tTelefone: %s\n", pessoa[indice].telefone);
+    printf("\tEmail: %s\n", pessoa[indice].email);
+    printf("\tObservacao: %s\n", pessoa[indice].obervacao);
+}
 
-    printf("Quantos contatos voce deseja adicionar?");
-    scanf("%d", &numero);
-    for (int i = 0; i < numero;i++){
-        printf("Informe o codigo: ");
-        scanf("%d", &pessoa[i].codigo);
-        printf("Informe o nome do contato: ");
-        scanf("%s", &pessoa[i].nome);
-        printf("Informe email:  ");
-        scanf("%s", &pessoa[i].email);
-        printf("Informe a data de nascimento:  ");
-        scanf("%s", &pessoa[i].data);
-        printf("Infome o telefone:  ");
-        scanf("%s", &pessoa[i].fone);
-        printf("Obervacao:  ");
-        scanf("%s", &pessoa[i].obseracao);
+char* alocarMemoria(int tamanho){
+    char* info = NULL;
+    info = (char*) malloc(sizeof(tamanho) * sizeof(char));
+
+    if(info == NULL){
+        printf("ERRO: impossivel alocar a quantidade de memoria requisitada!");
+        exit(1);
     }
+    return info;
+}
+
+void cadastrarContato(int registro){
+    char dados[MAX];
+    printf("\n\t\tNome: ");
+    scanf("%[^\n]s", dados);
+    setbuf(stdin, NULL);
+    //Alocação e cópia de dados
+    pessoa[registro].nome = alocarMemoria(strlen(dados));
+    strcpy(pessoa[registro].nome, dados);
+
+    printf("\t\tIdade: ");
+    scanf("%d", &pessoa[registro].idade);
+    setbuf(stdin, NULL);
+
+    printf("\t\tTelefone: ");
+    scanf("%[^\n]s", pessoa[registro].telefone);
+    setbuf(stdin, NULL);
+
+    printf("\t\tEmail: ");
+    scanf("%[^\n]s", dados);
+    setbuf(stdin, NULL);
+    //Alocação e cópia de dados
+    pessoa[registro].email = alocarMemoria(strlen(dados));
+    strcpy(pessoa[registro].email, dados);
+
+    printf("\t\tObservacao: ");
+    scanf("%[^\n]s", dados);
+    setbuf(stdin, NULL);
+    //Alocação e cópia de dados
+    pessoa[registro].obervacao = alocarMemoria(strlen(dados));
+    strcpy(pessoa[registro].obervacao, dados);
 
 }
 
-int lista(Pessoa pessoa[VET_Person],int count){
-    if (count==0){
-        printf("Nao a contatos!");
-        }else {
-            for (int i = 0; i < count; i++)
-            {
-                printf("\n%d", pessoa[i].codigo);
-                printf("\n%s", pessoa[i].nome);
-                printf("\n%s", pessoa[i].email);
-                printf("\n%s", pessoa[i].data);
-                printf("\n%s", pessoa[i].fone);
-                printf("\n%s", pessoa[i].obseracao);
+void listarContatos(int qtdeContatos){
+    int i;
+    int contagem = 1;
+    for(i = 0; i < qtdeContatos; i++){
+        printf("\n\tContato nº: %d\n", contagem++);
+        printf("\tNome: %s\n", pessoa[i].nome);
+        printf("\tIdade: %d\n", pessoa[i].idade);
+        printf("\tTelefone: %s\n", pessoa[i].telefone);
+        printf("\tEmail: %s\n", pessoa[i].email);
+        printf("\tObervacao: %s\n", pessoa[i].obervacao);
 
-            }
-            
+    }
+    printf("\n\n");
+}
+
+int pesquisarContato(int qtdeContatos, char* nomeBuscado){
+    int i;
+
+    for(i = 0; i < qtdeContatos; i++){
+        if(strcmp(nomeBuscado, pessoa[i].nome) == 0)
+            return i;
+    }
+    return -1;
+}
+
+int leitor(){
+    int opcao;
+
+    printf("\n\tOpcao escolhida: ");
+    scanf("%d", &opcao);
+    setbuf(stdin, NULL);
+
+    return opcao;
+}
+
+int main(){
+    char nome[MAX];
+    int numeroRegistro = 0;
+    int opcaoEscolhida;
+    int indice;
+
+    do{
+        exibirMenu();
+        opcaoEscolhida = leitor();
+
+        switch(opcaoEscolhida){
+            case 1:
+                cadastrarContato(numeroRegistro++);
+                break;
+            case 2:
+                listarContatos(numeroRegistro);
+                break;
+            case 3:
+                printf("\n\tNome: ");
+                scanf("%[^\n]s", nome);
+                indice = pesquisarContato(numeroRegistro, nome);
+
+                if(indice >= 0 && indice <= 99)
+                    exibirContato(indice);
+                else
+                    printf("\n\tContato não cadastrado!");
+                break;
+            case 4:
+                printf("\n\tSaindo...");
+                break;
+            default:
+                printf("Opção inválida!");
+                exibirMenu();
+                opcaoEscolhida = leitor();
         }
-    
-   
-}
-int main (){
 
-int count=0;
-
-int op; 
-  Pessoa pessoa[VET_Person];
-while (op!=0){
-
-    menu();
-
-    scanf("%d", &op);
-
-    switch (op){
-        case 1: 
-         count += cad(pessoa, count);
-          case 2:
-          lista(pessoa, count);
-
-    }
-
-}
-
+    }while(opcaoEscolhida != 4);
 }
